19ba1f870a8d24badb04a2e5c233ddc4cb67ca95
Michael Mol <mikemol@gmail.com>
2012-03-03 15:02:30 -0500

    first commit

---
+++ entbuff.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#include <linux/random.h>
+
+int looping = 1;
+
+int pos_write = 0;
+int buff_read_remaining = 0; // This many bytes left in the buffer
+int buff_size = 8388608;
+size_t read_in_count = 0;
+size_t read_out_count = 0;
+char* entbuff = 0;
+
+void unmap_ent()
+{
+	// unmap. Should be called by atexit() if we successfully mapped.
+	munmap(entbuff, buff_size);
+}
+
+FILE* fdRandom = NULL;
+
+void close_fdRandom()
+{
+	fclose(fdRandom);
+}
+
+void read_from_buffer()
+{
+	// If we don't have any bytes left to read, nullop.
+	if(0 == buff_read_remaining)
+		return;
+	
+	// Where in the buffer do we read from?
+	// pos_write is the next byte which will be written.
+	// So pos_write - 1 will be the last valid written byte.
+	int buff_read_pos = (pos_write - 1) - buff_read_remaining;
+	if(buff_read_pos < 0)
+		// Buffer wrapped.
+		buff_read_pos += buff_size;
+	
+	fwrite(entbuff + buff_read_pos, 1, 1, fdRandom);
+	int brr_w = buff_read_remaining;
+	--buff_read_remaining;
+	++read_out_count;
+}
+
+void write_to_buffer()
+{
+	// If our buffer is full, nullop.
+	if(buff_size == buff_read_remaining)
+		return;
+
+	// Buffer is wrapping.
+	if(pos_write >= buff_size)
+		pos_write -= buff_size;
+
+	fread(entbuff + pos_write, 1, 1, fdRandom);
+	int brr_w = buff_read_remaining;
+	int pw_w = pos_write;
+	++buff_read_remaining;
+	++pos_write;
+	++read_in_count;
+}
+
+int check_ent()
+{
+	int entcnt;
+        int r;
+	r = ioctl(fileno(fdRandom), RNDGETENTCNT, &entcnt);
+	if(0 > r) {
+		int e = errno;
+                fprintf(stderr, "Error with ioctl call: %s\n", strerror(e));
+		return -1;
+        }
+	return entcnt;
+}
+
+int main(int argc, char* argv[])
+{
+        int entcnt = 0;
+	int entthresh_high = 4096 * 1 / 2;
+	int entthresh_low = 4096 * 1 / 16;
+        int waittime = 62500;
+	int printperiod = 16;
+
+	entbuff = mmap(NULL, buff_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if(MAP_FAILED == entbuff)
+	{
+		int e = errno;
+                fprintf(stderr, "Error with mmap call: %s\n", strerror(e));
+		return -1;
+	}
+	if(NULL == entbuff)
+	{
+		int e = errno;
+		fprintf(stderr, "mmap returned NULL?!: %s\n", strerror(e));
+		return -1;
+	}
+
+	int at_res = atexit(unmap_ent); // We mapped, so unmap when we're done.
+	if(0 != at_res)
+	{
+		fprintf(stderr, "Warning: failed to register unmap_ent with atexit()");
+	}
+
+	fdRandom = fopen("/dev/random", "rw");
+	if(0 == fdRandom)
+	{
+		int e = errno;
+                fprintf(stderr, "Error with mmap call: %s\n", strerror(e));
+		return -1;
+	}
+
+	at_res = atexit(close_fdRandom); // We opened the file, remember to close it.
+	if(0 != at_res)
+	{
+		fprintf(stderr, "Warning: failed to register close_fdRandom with atexit()");
+	}
+
+        if(argc == 2) {
+                waittime = atoi(argv[1]);
+                if(waittime < 1) {
+                        fprintf(stderr, "specified wait time cannot be less than 1\n");
+                        return -1;
+                }
+        }
+	fprintf(stderr, "wait time is %d, high threshold is %d, low threshold is %d\n", waittime, entthresh_high, entthresh_low);
+        // loop here, calling ioctl(rfd, RNDGETENTCNT) and printing the result
+	entcnt = check_ent();
+	int printperiod_count = 0;
+	while( -1 != entcnt)
+	{
+                int ures = usleep(waittime);
+		if(printperiod_count++ >= printperiod)
+		{
+			fprintf(stderr, "%d bytes in buffer. %llu in, %llu out. %d -- %d\n", buff_read_remaining, (unsigned long long int) read_in_count, (unsigned long long int) read_out_count, ures, waittime);
+			printperiod_count = 0;
+		}
+		entcnt = check_ent();
+		int write_bytes_rmn = 0xffff; // how many bytes we allow ourselves to add to the buffer. 
+		int read_bytes_rmn = 0xffff; // How far we allow ourselves to draw down the buffer in one second.If we're not careful, we'll flood the pool and waste bytes.
+		while	(
+				(
+					((entcnt >= entthresh_high) && (buff_read_remaining != buff_size))
+					|| ((entcnt <= entthresh_low) && (buff_read_remaining != 0))
+				)
+				&& (write_bytes_rmn > 0)
+				&& (read_bytes_rmn > 0)
+			)
+		{
+			if(entcnt >= entthresh_high)
+			{
+				write_to_buffer();
+				write_bytes_rmn--;
+			}
+			else if(entcnt <= entthresh_low)
+			{
+				read_from_buffer();
+				read_bytes_rmn--;
+			}
+			else
+			{
+				fprintf(stderr, "BUG: entcnt(%d), etl(%d), eth(%d), brr(%d), bs(%d)\n", entcnt, entthresh_low, entthresh_high, buff_read_remaining, buff_size);
+			}
+			entcnt = check_ent();
+		}
+	}
+
+        return 0;
+}
---
+++ entropywatch.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <linux/random.h>
+
+int looping = 1;
+
+int main(int argc, char* argv[])
+{
+        int rfd = 0;
+        int entcnt = 0;
+        int r;
+        int waittime = 30;
+        char timebuf[512];
+        time_t now;
+        struct tm *tmp;
+
+        if(argc == 2) {
+                waittime = atoi(argv[1]);
+                if(waittime < 1) {
+                        fprintf(stderr, "specified wait time cannot be less than 1\n");
+                        return -1;
+                }
+        }
+
+        rfd = open("/dev/random", O_RDONLY);
+
+        if(-1 == rfd) {
+                fprintf(stderr, "error opening /dev/random\n");
+                return -1;
+        }
+
+        // loop here, calling ioctl(rfd, RNDGETENTCNT) and printing the result
+        do {
+                now = time(NULL);
+                r = ioctl(rfd, RNDGETENTCNT, &entcnt);
+                if(0 > r) {
+                        int e = errno;
+                        fprintf(stderr, "Error with ioctl call: %s\n", strerror(e));
+                        break;
+                }
+                tmp = localtime(&now);
+                strftime(timebuf, 512, "%F %T", tmp);
+                printf("[%s] %d\n", timebuf, entcnt);
+                sleep(waittime);
+        } while(looping);
+
+        close(rfd);
+        return 0;
+}
